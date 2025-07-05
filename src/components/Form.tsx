ba958351facf2185a9c3b6a6fa02cd00c4006366
a.keyghobadi <a123@s456>
2025-07-05 14:05:09 +0330

    form validation with hooks.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
 import { FormEvent, useRef, useState } from "react";
 import { useForm, FieldValues } from "react-hook-form";
+interface FormData {
+  name: string;
+  age: number;
+}
 const Form = () => {
-  const form = useForm();
-  const { register, handleSubmit } = form;
+  const form = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+  console.log("formState.errors :", errors);
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -12,22 +21,31 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">name is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">min length is 3 chars</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p className="text-danger">age is required</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
